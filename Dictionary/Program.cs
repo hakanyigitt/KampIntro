ccb7ce6d7d97017e98e86ab29e3cb393f9a7350a
Hakan YİĞİT <hakanyigit028@hotmail.com>
2022-02-06 00:07:45 +0300

    4.gün ödevler

---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> ogrenci = new Dictionary<int, string>();
+            ogrenci.Add(28, "Hakan Yiğit");
+            ogrenci.Add(53, "Eda Arıcı");
+            ogrenci.Add(37, "Büşra Nur Öz");
+
+            Console.WriteLine("Öğrenci No Giriniz: ");
+            int no = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(ogrenci[no]);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Öğrenci bulunamadı");
+            }
+
+        }
+    }
+}
