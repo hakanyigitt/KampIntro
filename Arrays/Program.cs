8a485db0b4fcf2018fb2a1cdacb96fbfa88530be
Hakan YİĞİT <hakanyigit028@hotmail.com>
2022-02-05 21:03:08 +0300

    4.gün ders sonu

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,19 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            
+            string[] students = new string[] { "Engin", "Derin", "Salih" };
+            /*               
+            students[0] = "Engin";
+            students[1] = "Derin";
+            students[2] = "Salih";
+            */
+
+            string[] students2 = { "Engin", "Derin", "Salih" };
+
+            foreach (string student in students2)
+            {
+                Console.WriteLine(student);
+            }
         }
     }
 }
